2e0bb32617923ebbe701a038fea72693515eabc7
sk4cks <109573477+sk4cks@users.noreply.github.com>
2022-12-01 16:08:51 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,3 @@
+-- 코드를 입력하세요
+SELECT member_id, member_name, gender, to_char(date_of_birth,'yyyy-mm-dd') date_of_birth from member_profile
+where to_char(date_of_birth,'mm') = '03' and tlno is not null and gender='W' order by member_id asc
