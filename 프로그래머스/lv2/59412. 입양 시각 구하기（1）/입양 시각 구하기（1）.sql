e13ac284ea772e6f2ab3ef2436bbd0f6faa6aad8
나카롱 <109573477+sk4cks@users.noreply.github.com>
2022-12-15 17:21:52 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -0,0 +1,2 @@
+-- 코드를 입력하세요
+SELECT to_number(to_char(datetime,'hh24')) hour,count(*) count from animal_outs where to_char(datetime,'hh24')>=9 and to_char(datetime,'hh24')<=19 group by to_char(datetime,'hh24') order by hour asc
